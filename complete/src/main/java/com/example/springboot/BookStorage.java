0806bdb5efa24477690eef41f404781804424049
JustinStri <jstrikowski@ufl.edu>
2022-04-07 03:44:14 -0400

    Recent progress. Created book object and functions. Need to work on frontend and input/storage.

---
+++ complete/src/main/java/com/example/springboot/BookStorage.java
@@ -0,0 +1,9 @@
+import java.util.Map;
+import java.util.Vector;
+import java.util.Map;
+
+class BookStorage {
+    Map bookShelf; 
+    //working on this,
+
+}
--- complete/src/main/java/com/example/springboot/HelloController.java
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -1,14 +1,61 @@
 package com.example.springboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+//sep
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.ui.ModelMap;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
 
 @RestController
 public class HelloController {
 
-	@GetMapping("/")
+	@GetMapping("/test/")
 	public String index() {
-		return "Greetings from Spring Boot!";
+		return "Greetings from Spring Boot! testing changes";
 	}
+
+	@GetMapping("/book/")
+	public String getBooks() {
+		return "The book you want!";
+	}
+
+	@GetMapping("/book/")
+	public String getBook() {
+		return "The book you want!";
+	}
+
+	@GetMapping("/book/")
+	public String createBook() {
+		return "Create new book";
+	}
+// 	@GetMapping("/greeting")
+//   public String greetingForm(Model model) {
+//     model.addAttribute("greeting", new Book());
+//     return "greeting";
+  
+
+
+	// @GetMapping("/addBook/")
+	// public String addBook(@ModelAttribute("SpringWeb")Book theBook, 
+	//    ModelMap model) {
+	//    model.addAttribute("title", theBook.getTitle());
+	//    model.addAttribute("author", theBook.getAuthor();
+	//    model.addAttribute("ISBN", theBook.getISBN());
+	   
+	//    return "result";
+	// }
+// 	@RequestMapping("/student/")
+//    	public Test student() {
+// 		return "Test";
+//       //return new Book("title1", 12345, "Hesse");
+//    }
 
 }
